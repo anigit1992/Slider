583430cb71d3f1e2a56bf3d2c163a393a4bfe9d1
Ani <ani92S@mail.ru>
2017-04-17 19:05:55 +0400

    initial commit

--- test.js
+++ test.js
@@ -5,23 +5,43 @@ var n=img.length;
 var index=0;
 var k=document.querySelector('#k');
 k.style.display=`block`;
+// previousBtn.addEventListener('keydown', a.keyCode);
 previousBtn.addEventListener('click', a);
 function a() {
-    if (index > 0) {
+    if (index>0) {
         index--;
-        if (index < n) {
-            img[index].style.display = `block`;
-            img[index + 1].style.display = `none`;
+        console.log(index);
+        if(index<n ){
+            img[index].style.display=`block`;
+            img[index+1].style.display=`none`;
         }
     }
-    nextBtn.addEventListener('click', nextClic);
-    function nextClic() {
-        if (index < n - 1) {
-            index++;
-            if (index < n) {
-                img[index].style.display = `block`;
-                img[index - 1].style.display = `none`;
-            }
+    if(index==0){
+        previousBtn.style.display=`none`;
+    }
+    if(index!=n-1){
+        nextBtn.style.display=`inline-block`;
+    }
+}
+
+nextBtn.addEventListener('keydown', nextClic.keyCode);
+nextBtn.addEventListener('click',nextClic );
+function nextClic() {
+    if (index<n-1) {
+        index++;
+        console.log(index);
+        if(index<n){
+            img[index].style.display=`block`;
+            img[index-1].style.display=`none`;
         }
+    }
+    if(index>0){
+        previousBtn.style.display=`inline-block`;
+    }
+    if(index==n-1){
+        nextBtn.style.display=`none`;
+    }
+    if(index!=n-1){
+        nextBtn.style.display=`inline-block`;
     }
 }
